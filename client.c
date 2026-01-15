6bc4ff66308128b39c7051154af02d6d6c6e1e76
yu <ylu60@stuy.edu>
2026-01-15 07:48:54 -0500

    removed olf func from lab + made server sent text safer

--- client.c
+++ client.c
@@ -1,40 +1,63 @@
 #include "networking.h"
 
-int main(int argc, char *argv[]) {
-  char* IP = "127.0.0.1";
-  if(argc > 1) IP = argv[1];
+void clientLogic(int server_socket){
+  char msg[BUFFER_SIZE];
+  char response[BUFFER_SIZE];
+  memset(msg, 0, sizeof(msg));
+  memset(response, 0, sizeof(response));
 
+  printf("Enter a message:\n");
+  if((fgets(msg, sizeof(msg), stdin)) == NULL){
+    exit(0);
+  }
+
+  // Send to server
+  send(server_socket, msg, strlen(msg), 0);
+
+  // Wait for response
+  if (recv(server_socket, response, sizeof(response), 0) > 0) {
+      printf("Server says: %s\n", response);
+  }
+}
+
+int main(int argc, char *argv[] ) {
+  char* IP = "127.0.0.1";
+  if(argc>1){
+    IP=argv[1];
+  }
   int server_socket = client_tcp_handshake(IP);
-  printf("Connected to server! Waiting for game...\n");
+  printf("client connected.\n");
 
   fd_set read_fds;
-  char buff[BUFFER_SIZE];
-
   while(1){
     FD_ZERO(&read_fds);
-    FD_SET(STDIN_FILENO, &read_fds);
+    FD_SET(fileno(stdin), &read_fds); // select from user input and server
     FD_SET(server_socket, &read_fds);
 
-    if (select(server_socket + 1, &read_fds, NULL, NULL, NULL) == -1) {
+    if (select(server_socket + 1, &read_fds, NULL, NULL, NULL) == -1) { // server is proly max, but if it breaks rip
       perror("select");
       exit(1);
     }
 
-    // Message from Server
+    // server messages
     if(FD_ISSET(server_socket, &read_fds)){
+      char buff[BUFFER_SIZE];
       memset(buff, 0, sizeof(buff));
+
       int bytes = recv(server_socket, buff, sizeof(buff) - 1, 0);
 
+      
       if(bytes <= 0){
-        printf("\nDisconnected from server.\n");
+        printf("You died!\n"); // should probably do like a last message to print stats or something before exit 0ing
         exit(0);
       }
-      printf("%s", buff);
-      fflush(stdout); // Ensure text appears immediately
+      printf("\n%s\n", buff);
+      fflush(stdout);
     }
 
-    // Input from User
+    // user inputs
     if(FD_ISSET(STDIN_FILENO, &read_fds)){
+      char buff[BUFFER_SIZE];
       memset(buff, 0, sizeof(buff));
       if(fgets(buff, sizeof(buff), stdin)){
         buff[strcspn(buff, "\r\n")] = 0;
@@ -42,4 +65,5 @@ int main(int argc, char *argv[]) {
       }
     }
   }
+  close(server_socket);
 }
